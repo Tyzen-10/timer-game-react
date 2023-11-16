be55ab9865780fa9bba49465aa55d866b6f23f69
Venkatesan R S <wenkatesan@gmail.com>
2023-11-16 14:24:26 +0530

    commit before new exp branch.

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -4,20 +4,33 @@ import ResultModal from "./ResultModal";
 export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
   const dialog = useRef();
-  const [timerStarted, setTimerStarted] = useState(false);
+  
   const [timerExpired, setTimerExpired] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(targetTime*1000)
+  // timer is active if not 0 and not equal to target time. i.e.stop and start time.
+  const timerIsActive = timeRemaining>0 && timeRemaining<targetTime*1000
+  //console.log(timerIsActive)
+  //timer expires.
+  if (timeRemaining<=0){
+    clearInterval(timer.current)
+    setTimeRemaining(targetTime*1000)
+    dialog.current.open()
+  }
   const handleStart = () => {
-    timer.current = setTimeout(() => {
-      setTimerExpired(true);
-      dialog.current.open();
-    }, targetTime * 1000);
-
-    setTimerStarted(true);
+    timer.current = setInterval(() => {
+        //two params, one the time interval and the second one for the function that should be executed every interval.
+      setTimeRemaining(prevTimeRemaining=>prevTimeRemaining-10)
+    }, 10);
   };
   const handleStop = () => {
-    clearTimeout(timer.current);
-    //remove if needed. not in udemy course.
-    setTimerStarted(false);
+    dialog.current.open();
+    clearInterval(timer.current);
+    //added piece of code by me- since timerisActive not false when stopped manually.
+    //will be solved in later video because we write seperate function for reset.
+    //remove below two lines after handleReset.
+    setTimeRemaining(targetTime*1000)
+    console.log(timerIsActive)
+    
   };
   return (
     <>
@@ -28,12 +41,12 @@ export default function TimerChallenge({ title, targetTime }) {
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
         <p>
-          <button onClick={timerStarted ? handleStop : handleStart}>
-            {timerStarted ? "Stop" : "Start"}
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop" : "Start"}
           </button>
         </p>
-        <p className={timerStarted ? "active" : "undefined"}>
-          {timerStarted ? "Timer running" : "Timer inactive"}
+        <p className={timerIsActive ? "active" : "undefined"}>
+          {timerIsActive ? "Timer running" : "Timer inactive"}
         </p>
       </section>
     </>
